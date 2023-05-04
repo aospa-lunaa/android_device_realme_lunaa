10eceee1f777b047d8c4fbca3941c7f9614cf6cd
rk134 <me@rk134.cf>
2023-05-04 13:16:57 +0100

    lunaa: Sync with latest vendor/oplus/camera repository
    * Most of these things have been moved to vendor/oplus/camera, to avoid conflict with other devices.
    
    Signed-off-by: rk134 <me@rk134.cf>

--- device.mk
+++ device.mk
@@ -244,19 +244,8 @@ PRODUCT_PACKAGES += \
     OPlusCarrierConfig \
     WifiResTarget
 
-# Oplus camera
-$(call inherit-product, vendor/oplus/camera/camera-vendor.mk)
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/oplus_camera_default_grant_permissions_list.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/default-permissions/oplus_camera_default_grant_permissions_list.xml \
-    $(LOCAL_PATH)/configs/permissions/oplus_google_lens_config.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/oplus_google_lens_config.xml \
-    $(LOCAL_PATH)/configs/permissions/privapp-permissions-oplus.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-oplus.xml \
-    $(LOCAL_PATH)/configs/sysconfig/hiddenapi-package-oplus-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package-oplus-whitelist.xml
-
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.vendor.camera.privapp.list=com.oplus.camera \
-    ro.com.google.lens.oem_camera_package=com.oplus.camera \
-    ro.com.google.lens.oem_image_package=com.oneplus.gallery
+# OplusCamera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
 
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.oplus.system.camera.name=com.oplus.camera \
@@ -264,17 +253,6 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
     ro.oplus.camera.video_beauty.prefix=oplus.video.beauty. \
     ro.oplus.camera.video.beauty.switch=oplus.switch.video.beauty \
     ro.oplus.camera.speechassist=true
-
-# Oplus framework
-PRODUCT_PACKAGES += \
-    oplus-fwk
-
-PRODUCT_BOOT_JARS += \
-    oplus-fwk
-
-# Oplus wrapper
-PRODUCT_BOOT_JARS += \
-    oplus-support-wrapper
 
 # ParanoidDoze
 PRODUCT_PACKAGES += \
